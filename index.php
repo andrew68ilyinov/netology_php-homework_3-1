6eb65ca2d0cf61489dcf471f44c17366b736f881
Andrew Ilyinov <ai68_68@mail.ru>
2018-07-25 23:38:06 +0300

    Добавил классы и экземпляры

--- index.php
+++ index.php
@@ -36,9 +36,13 @@ DOC;
 
 # Задание 3.
 
-class Cars // Описание класса машин
+/*
+ *  1) Автомобили
+ */
+
+class Cars
 {
-    const WHEELS = 4;        // 4 колеса
+    const WHEELS = 4;  // 4 колеса
     
     public $brand;    // Марка, напр. 'Toyota'
     public $model;    // Модель (Rav4)
@@ -46,7 +50,7 @@ class Cars // Описание класса машин
     public $complect; // Комплектация
     public $price;    // Цена
     
-    public function __construct($brand = 1, $model = 1, $color = 'Yellow', $complect = 25, $price = 100)
+    public function __construct($brand = 1, $model = 1, $color = 'Yellow', $complect = 25, $price = 100) // Инициализируем любыми знач. по умолч.
     {
         $this->brand    = $brand;
         $this->model    = $model;
@@ -57,7 +61,7 @@ class Cars // Описание класса машин
     
     public function getPrice($discount)
     {
-        if ($discount = 0) {
+        if ($discount === 0) {
             $finalPrice = $this->price;
         } else {
             $finalPrice = $this->price * $discount / 100;
@@ -67,7 +71,7 @@ class Cars // Описание класса машин
     }
 }
 
-$car1 = new Cars();
+$car1 = new Cars(); // Создаю 2 объекта двумя способами
 $car1->brand    = 'Toyota';
 $car1->model    = 'Rav4';
 $car1->color    = 'White';
@@ -80,3 +84,46 @@ echo '<br>' . 'Итоговая цена машины марки Toyota: ' . $ca
 echo '<br>' . 'Итоговая цена машины марки BMW: ' . $car2->getPrice(5); // Дадим скидку 5 процентов
 
 
+/*
+ *  2) Телевизоры
+ */
+
+class TV
+{
+    public $brand;
+    public $model;
+    public $diagonal;
+    public $resolution;
+    public $price;
+    
+    public function __construct($brand = 1, $model = 1, $diagonal = 40, $resolution = 'HD', $price = 1000)
+    {
+        $this->brand       = $brand;
+        $this->model       = $model;
+        $this->diagonal    = $diagonal;
+        $this->resolution  = $resolution;
+        $this->price       = $price;
+    }
+    
+    public function getTV()
+    {
+        echo 'Вы получили: ' . $this->brand . ' ' . $this->model . ' ' . $this->diagonal . ' ' . $this->resolution . ' ' . $this->price;
+    }
+}
+
+$tv1 = new TV('Sony', 'KD-55XF8577', '55', 'Ultra HD', 110000);
+$tv2 = new TV('Samsung', 'UE40MU6470U', '40', 'Ultra HD', 40000);
+
+
+/*
+ *  3) Шариковая ручка
+ */
+
+class Pens
+{
+    public $brand;
+    public $color;
+    public $widthWright; // Толщина линии письма
+    
+    
+}
